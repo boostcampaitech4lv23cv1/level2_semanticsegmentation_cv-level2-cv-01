fe43214a4439934e045affa53abd602115e25d2c
Yoonjae Cho <nicolasc0714@gmail.com>
2022-12-26 11:39:36 +0000

    add dataset, fit to training for vit adapter

--- ViT-Adapter/segmentation/configs/_base_/default_runtime.py
+++ ViT-Adapter/segmentation/configs/_base_/default_runtime.py
@@ -2,13 +2,22 @@
 log_config = dict(
     interval=50,
     hooks=[
-        dict(type='TextLoggerHook', by_epoch=False),
-        # dict(type='TensorboardLoggerHook')
-    ])
+        dict(type="TextLoggerHook", by_epoch=False),
+        dict(
+            type="WandbLoggerHook",
+            interval=1000,
+            init_kwargs=dict(
+                entity="kidsarebornstars",
+                project="segmentation",
+                name="mask2former_beit_adapter_large_896_80k_cityscapes_ss",
+            ),
+        ),
+    ],
+)
 # yapf:enable
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
+dist_params = dict(backend="nccl")
+log_level = "INFO"
 load_from = None
 resume_from = None
-workflow = [('train', 1)]
+workflow = [("train", 1)]
 cudnn_benchmark = True
---
+++ ViT-Adapter/segmentation/configs/_base_/schedules/schedule.py
@@ -0,0 +1,24 @@
+lr = 1e-4
+
+# optimizer
+optimizer = dict(type="AdamW", lr=lr, weight_decay=0.01)
+optimizer_config = dict()
+
+# learning policy
+lr_config = dict(
+    policy="CosineAnnealing",
+    warmup="linear",
+    warmup_iters=300,
+    warmup_ratio=1.0 / 10,
+    min_lr_ratio=5e-6,
+)
+
+# runtime settings
+runner = dict(type="IterBasedRunner", max_iters=3000)
+checkpoint_config = dict(by_epoch=False, interval=300)
+evaluation = dict(
+    interval=300,
+    metric="mIoU",
+    save_best="mIoU",
+    classwise=True,
+)
